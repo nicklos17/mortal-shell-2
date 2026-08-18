4bd6dd0837578e54e7883103c005a329f423c154
lixiaojun <842616717@qq.com>
2026-08-18 08:52:02 +0800

    feat:隐私政策

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/privacy-policy",
+        destination: "/privacy",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
